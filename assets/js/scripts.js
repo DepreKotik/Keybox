6350a1787fa5d838ecac6426baf53f7c36dc0b05
DepreKotik <vovachegotov@gmail.com>
2023-09-26 13:45:12 +0300

    вносит правки по главной странице

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,7 @@
-const questionsItems = document.querySelectorAll('.questions__item');
 const header = document.querySelector('.header');
 const upButton = document.querySelector('.up-button');
 let oldValue;
 
-questionsItems.forEach((element) => {
-  element.addEventListener('click', (event) => {
-    if(event.target.classList.contains('questions__button')) {
-      element.querySelector('.questions__answer').classList.toggle('questions__item--open');
-      element.querySelector('.questions__button').classList.toggle('questions__button--open');
-    }
-  })
-});
 
 upButton.addEventListener('click', () => {
   window.scrollTo({
@@ -29,8 +20,7 @@ const setUpButtonState = (value) => {
 
 window.addEventListener('scroll', () => {
   let newValue = window.pageYOffset;
-  console.log(newValue);
-  if (oldValue > newValue || newValue <= 0) {
+  if (oldValue > newValue || newValue === 0) {
     setHeaderState('0')
   } else {
     setHeaderState('-100%')
