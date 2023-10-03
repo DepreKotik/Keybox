533127166a956f95ac29a9ab08d08215ea70eee3
DepreKotik <vovachegotov@gmail.com>
2023-10-03 18:01:28 +0300

    вносит правки по шрифтам и фокусам

--- assets/js/init-map.js
+++ assets/js/init-map.js
@@ -3,7 +3,7 @@ import {getData} from './api.js';
 const URL = 'https://my-json-server.typicode.com/DepreKotik/json-bd';
 const START_POINT = {lat: 57.152985, lng: 65.541227};
 const START_ZOOM = 13;
-const LAYER_URL = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const LAYER_URL = 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png';
 
 const warehouseMap = document.querySelector('.warehouse__map');
 const map = new L.map(warehouseMap);
